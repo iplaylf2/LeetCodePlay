cb3694ca371fb6b76ed32b043a15b50cc8e2f0a4
IplayLF2 <914637118@qq.com>
2020-01-20 19:09:12 +0800

    trivial

--- draft/Untitled-1.js
+++ draft/Untitled-1.js
@@ -2,206 +2,53 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
-var solveSudoku = function(board) {
-  const rowArray = nineNull.concat(0).map(() => new Bar()),
-    columnArray = nineNull.concat().map(() => new Bar()),
-    boxArray = nineNull.concat().map(() => new Box());
+var solveSudoku = function(board) {};
 
-  var whiteArray = recordExisting(board, rowArray, columnArray, boxArray);
-};
+const $9zero = new Array(9).fill(0);
+const $9item = new Array(9).fill(null);
+const indexMap = $9item.map((_, y) =>
+  $9zero.map((_, x) => Math.floor(y / 3) * 3 + Math.floor(x / 3))
+);
 
-/**
- * @param {character[][]} board
- * @param {Bar[]} rowArray
- * @param {Bar[]} columnArray
- * @param {Box[]} boxArray
- * @param {White[]} whiteArray
- */
-const recordExisting = function(board, rowArray, columnArray, boxArray) {
-  /**
-   * @type {White[]}
-   */
-  const whiteArray = [];
-
-  for (var y = 0; y !== 9; y++) {
-    for (var x = 0; x !== 9; x++) {
-      const digit = board[y][x],
-        row = rowArray[y],
-        column = columnArray[x],
-        box = boxArray[boxMap[y][x]];
-      if (digit === ".") {
-        const white = new White(x, y, column, row, box);
-        whiteArray.push(white);
-      } else {
-        row.existing.record(digit);
-        column.existing.record(digit);
-        box.existing.record(digit);
-      }
-    }
-  }
-
-  return whiteArray;
-};
-
-class Waiting {
+//cell排除，cell的可选项被一步步排除至最后一项，就是cell的值。
+class UnitExclusive {
   constructor() {
-    this.bit = 1;
+    this.bitmap = 0b1111111110;
     this.length = 9;
   }
 
-  /**
-   * @param {number} digit
-   */
-  reduce(digit) {
-    this.bit |= 1 << digit;
-    this.length--;
-  }
-
-  /**
-   * @param {number} digit
-   */
-  reduceWithCheck(digit) {
+  push(digit) {
     const digitBit = 1 << digit;
-    if (this.bit & digitBit) {
-      this.bit |= digitBit;
+    if (this.bitmap & digitBit) {
+      this.bitmap ^= digitBit;
       this.length--;
-    }
-  }
-
-  hasUniqueIndex() {
-    return this.length === 1;
-  }
-
-  getUniqueIndex() {
-    return Math.log2(this.bit ^ 0b11_1111_1111);
-  }
-}
-
-class Existing {
-  constructor() {
-    this.bit = 0;
-    /**
-     * @type {number[]}
-     */
-    this.list = [];
-  }
-
-  /**
-   * @param {number} digit
-   */
-  record(digit) {
-    this.bit |= 1 << digit;
-    this.list.push(digit);
-  }
-
-  /**
-   * @param {number} digit
-   */
-  recordWithCheck(digit) {
-    const digitBit = 1 << digit;
-    if ((this.bit & digitBit) === 0) {
-      this.bit |= digitBit;
-      this.list.push(digit);
-    }
-  }
-
-  get length() {
-    return this.list.length;
-  }
-}
-
-class Exclusion {
-  constructor() {
-    this.bit = 0;
-    this.length = 0;
-  }
-
-  /**
-   * @param {number} index
-   */
-  record(index) {
-    this.bit |= 1 << index;
-    this.length++;
-  }
-
-  /**
-   * @param {number} index
-   */
-  recordWithCheck(index) {
-    const indexBit = 1 << index;
-    if ((this.bit & indexBit) === 0) {
-      this.bit |= indexBit;
-      this.length++;
-    }
-  }
-
-  /**
-   *
-   * @param {number} a
-   * @param {number} b
-   * @param {number} c
-   */
-  checkBar(a, b, c) {
-    const indexBit = ((1 << a) + (1 << b) + (1 << c)) ^ 0b1_1111_1111;
-    if ((this.bit & indexBit) === indexBit) {
-      return true;
+      return length === 1 ? Math.log2(this.bitmap) : NaN;
     } else {
-      return false;
+      return NaN;
     }
-  }
-
-  hasUniqueIndex() {
-    return this.length === 8;
-  }
-
-  getUniqueIndex() {
-    return Math.log2(this.bit ^ 0b1_1111_1111);
   }
 }
 
-class White {
-  /**
-   * @param {numnber} x
-   * @param {number} y
-   * @param {Bar} column
-   * @param {Bar} row
-   * @param {Box} box
-   */
-  constructor(x, y, column, row, box) {
+class Cell extends UnitExclusive {
+  constructor(x, y) {
+    super();
     this.x = x;
     this.y = y;
-    this.column = column;
-    this.row = row;
-    this.box = box;
-    this.wating = new Waiting();
-    this.fixed = false;
   }
 }
 
-class Bar {
+//block排除，block有9个cell，如一个数在其他八个cell中被排除，另外一个cell的值就是该数。
+class Block {
   constructor() {
-    this.existing = new Existing();
-    /**
-     * @type {Map<number,Exclusion>}
-     */
-    this.exclusionMap = new Map();
+    this.waitingValue = $9item.map(() => new UnitExclusive());
+  }
+
+  push(value, position) {
+    const waiting = this.waitingValue[value];
+    if (waiting === null) {
+      return NaN;
+    } else {
+      return waiting.push(position);
+    }
   }
 }
-
-class Box {
-  constructor() {
-    this.existing = new Existing();
-    /**
-     * @type {Map<number,Exclusion>}
-     */
-    this.exclusionMap = new Map();
-  }
-}
-
-const nineZero = new Array(9).fill(0);
-const nineNull = new Array(9).fill(null);
-const boxMap = nineNull
-  .concat()
-  .map((_, y) =>
-    nineZero.concat().map((_, x) => Math.floor(y / 3) * 3 + Math.floor(x / 3))
-  );
